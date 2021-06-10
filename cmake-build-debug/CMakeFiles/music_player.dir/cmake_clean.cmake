d00a21045b47ae91bc56625b8f0f29deefc61545
darkr00t <penialtoorpolute881@gmail.com>
2021-06-10 00:44:21 -0400

    And so it begins...

---
+++ cmake-build-debug/CMakeFiles/music_player.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ver2.dir/main.c.obj"
+  "libver2.dll.a"
+  "ver2.exe"
+  "ver2.exe.manifest"
+  "ver2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ver2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -5,81 +5,93 @@
 #include <unistd.h>
 
 //INTERNAL LIBS
-#include "raylib.h"
+//#include "raylib.h"
 #include "fmod/api/lowlevel/inc/fmod.h"
 
 //UTILITY
 char *direcGen();
-char* newDirec(char* direc, char* input);
+
+char *newDirec(char *direc, char *input);
+
 char *strremove(char *str, char *sub);
 
 //SONG
 void PLAY_SONG(char *SOUND_FILE_PATH);
-char* REMOVE_SONG(char *direc, char* song);
-char* SONG_DIREC(char* direc, char* song_name);
+
+char *REMOVE_SONG(char *direc, char *song);
+
+char *SONG_DIREC(char *direc, char *song_name);
 
 //GUI
-void CREATE_GUI();
 
-int main(void) {//TODO STOP PROGRAM FROM CLOSING AT END OF SONG
+
+int main(void) {
 
     //INITIALIZING VARIABLES
-    char x[1] = {'/'};
-    char* def = direcGen();
-    char* direc = (char*)malloc(sizeof(char) * (strlen(def) + 1));
+    int flag = 1;
+    char x[2] = {'/'};
+    char *def = direcGen();
+    char *direc = (char *) malloc(sizeof(char) * (strlen(def) + 1));
     char *input = (char *) malloc(sizeof(char) * 100);
 
     strcpy(direc, def);
-    while(1){
-        //waiting for command
-        scanf("%s", input);
-        if(strcmp(input,"-direc") == 0){
+    while (1) {
+        if (flag < 0);
+        else scanf("%s", input);
+        if (strcmp(input, "-help") == 0) {
+            printf(
+                    "\n------------------------------------------------------------------------------------------------------------------------\n"
+                    "DIRECTORY OPTIONS:\n    -direc\n\t\t-loc\n\t\t-default\n\t\t<PATH>\n\n"
+                    "PLAY OPTIONS:\n    -play <SONG_NO_SPACE>.mp3\t\t\t\t<--------------------------------------------[MUST INCLUDE .mp3]\n\n"
+                    "EXIT:\n    -exit\n"
+                    "\n------------------------------------------------------------------------------------------------------------------------\n"
+            );
+            while (getchar() != '\n');
+        } else if (strcmp(input, "-direc") == 0) {
             scanf("%s", input);
-            if(strcmp(input,"-default") == 0){
-                printf("DEFAULT: %s\n",def);
-                direc = strcpy(direc,def);
+            if (strcmp(input, "-loc") == 0) {
+            } else if (strcmp(input, "-default") == 0) {
+                //printf("DEFAULT: %s\n", def);
+                direc = strcpy(direc, def);
             } else {
                 strcat(input, x);
+                printf("%s\n", input);
                 direc = newDirec(direc, input);
-                printf("%llu %s\n", strlen(direc), direc);
             }
-        } else if(strcmp(input,"-play") == 0){
-            scanf("%s",input);
+            while (getchar() != '\n');
+            printf("\nCURRENT WORKING DIRECTORY: %s\n\n", direc);
+        } else if (strcmp(input, "-play") == 0) {
+            scanf("%s", input);
             direc = SONG_DIREC(direc, input);
-            CREATE_GUI();
             PLAY_SONG(direc);
             direc = REMOVE_SONG(direc, input);
-            //printf("%s\n",direc);
-        } else if(strcmp(input,"-exit") == 0){
+            while (getchar() != '\n');
+            //TODO READD -stop FEATURE
+            //TODO -play -all
+        } else if (strcmp(input, "-exit") == 0) {
             break;
         } else {
-            printf("\nCOMMAND NOT UNDERSTOOD...\n");
-        } while ((getchar()) != '\n');
+            printf("COMMAND NOT UNDERSTOOD...\n");
+        }
     }
 }
 
 //-------SONG-----------------------------------------------------------------------------------------------------------
-
-char* SONG_DIREC(char* direc, char* song_name) {
-    //LENGTH OF direc
-    unsigned long long length = 1 + strlen(direc);
-
+char *SONG_DIREC(char *direc, char *song_name) {
     //REFORMATTING SONG DIRECTORY
     strcat(direc, song_name);//<-----
 
     // replaces '\' w/ '/' for FMOD to work properly
-    for (int i = 0; i < length; i++)
+    unsigned long long length = 1 + strlen(direc);
+    for (int i = 0; i < length; i++) {
         if (direc[i] == '\\') direc[i] = '/';
+    }
 
     return direc;
 }
 
 void PLAY_SONG(char *SOUND_FILE_PATH) {
-    while(1) {
-        char temp[6];
-        char x;
-
-
+    while (1) {
         FMOD_SYSTEM *system;
 
         FMOD_RESULT err = FMOD_System_Create(&system);
@@ -91,18 +103,12 @@ void PLAY_SONG(char *SOUND_FILE_PATH) {
         FMOD_SOUND *sound;
         err = FMOD_System_CreateSound(system, SOUND_FILE_PATH, FMOD_HARDWARE, 0, &sound);
         if (err != 0) break;
-        //SONG BEGINS PLAYING HERE
+
         FMOD_CHANNEL *channel;
         FMOD_System_PlaySound(system, sound, 0, 0, &channel);
 
         FMOD_BOOL isPlaying = 1;
-        while (isPlaying){
-            scanf("%s",temp);
-            if(strcmp(temp,"-stop") == 0){//while((x=getchar()!='\n')&& x != EOF);
-                break;
-            } else {
-
-            }
+        while (isPlaying) {
             FMOD_Channel_IsPlaying(channel, &isPlaying);
         }
 
@@ -116,8 +122,8 @@ void PLAY_SONG(char *SOUND_FILE_PATH) {
     }
 }
 
-char* REMOVE_SONG(char *direc, char* song){
-    strremove(direc,song);
+char *REMOVE_SONG(char *direc, char *song) {
+    strremove(direc, song);
     return direc;
 }
 
@@ -125,32 +131,30 @@ char* REMOVE_SONG(char *direc, char* song){
 
 
 
-//-------UTILITY--------------------------------------------------------------------------------------------------------
 
-char* direcGen(){
+//-------UTILITY--------------------------------------------------------------------------------------------------------
+char *direcGen() {
     int MAX_BUF = 200;
-    char* direc = (char*)malloc(sizeof(char) * MAX_BUF);
+    char *direc = (char *) malloc(sizeof(char) * MAX_BUF);
 
     //GETTING AND STORING CURRENT WORKING DIRECTORY
     strcpy(direc, getcwd(direc, MAX_BUF));
-    strcpy(direc, strremove(direc, "\\cmake-build-debug-mingw"));// debug purposes only
+    strcpy(direc, strremove(direc, "\\cmake-build-debug"));// debug purposes only
     strcat(direc, "\\music_files\\");
 
     unsigned long long length = 1 + strlen(direc);
-
     for (int i = 0; i < length; i++)
         if (direc[i] == '\\') direc[i] = '/';
 
     return direc;
 }
 
-char* newDirec(char* direc, char* input){
+char *newDirec(char *direc, char *input) {
     strcpy(direc, input);
-    realloc(direc,sizeof(char) * (strlen(input) + 2));
+    realloc(direc, sizeof(char) * strlen(input) + 1);
 
     unsigned long long length = 2 + strlen(input);
-
-    for (int i = 0; i < length - 1; i++){
+    for (int i = 0; i < length - 1; i++) {
         if (direc[i] == '\\') direc[i] = '/';
     }
 
@@ -159,15 +163,14 @@ char* newDirec(char* direc, char* input){
 
 char *strremove(char *str, char *sub) {
     char *p, *q, *r;
+
     if ((q = r = strstr(str, sub)) != NULL) {
         size_t len = strlen(sub);
         while ((r = strstr(p = r + len, sub)) != NULL) {
-            while (p < r)
-                *q++ = *p++;
+            while (p < r) *q++ = *p++;
         }
-        while ((*q++ = *p++) != '\0')
-            continue;
+        while ((*q++ = *p++) != '\0');
     }
+
     return str;
 }
-
